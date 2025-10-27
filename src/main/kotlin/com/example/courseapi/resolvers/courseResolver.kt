15e34b04be2e6f9713c384b9407850b46c66a5e4
das-1102 <tomdh@miamioh.edu>
2025-10-27 14:44:51 -0400

    added unexpected exception also my commits werent working what the heck

--- src/main/kotlin/com/example/courseapi/resolvers/courseResolver.kt
+++ src/main/kotlin/com/example/courseapi/resolvers/courseResolver.kt
@@ -18,6 +18,7 @@ class CourseResolver(private val service: CourseService, private val logger: Log
         catch (e: TokenException) { logger.error("Token Exception: Couldn't fetch token (returned empty)"); CourseResult.Error("TOKEN EXCEPTION", e.message) }
         catch (e: QueryException) { logger.error("Query Exception: Query returned too many results"); CourseResult.Error("QUERY EXCEPTION", e.message) }
         catch (e: IllegalArgumentException) { logger.error("Illegal Argument Exception: Subjects empty or invalid"); CourseResult.Error("ILLEGAL ARGUMENT EXCEPTION", e.message) }
+        catch (e: Exception) {logger.error("Unexpected Exception: ${e.message}"); CourseResult.Error("UNEXPECTED EXCEPTION", e.message) }
     }
 
 //    @QueryMapping
