0105d5ba6dced5a157217e7c64b0344e302d7426
das-1102 <tomdh@miamioh.edu>
2025-11-17 22:31:08 -0500

    made coursenum and subject go hand in hand

--- src/main/kotlin/com/example/courseapi/services/course/courseService.kt
+++ src/main/kotlin/com/example/courseapi/services/course/courseService.kt
@@ -15,7 +15,10 @@ class CourseService(private val repo: CourseRepo) {
 
         if (campus.isEmpty() || !campus.all { it in fields.campuses }) throw IllegalArgumentException("Campuses empty or invalid")
         if (term.isEmpty() || term !in fields.terms) throw IllegalArgumentException("Term is empty or invalid")
-        if (!(subject.isNullOrEmpty() || subject.all { it in fields.subjects })) throw IllegalArgumentException("Invalid subjects field")
+        if (!(subject.isNullOrEmpty() || subject.all { it in fields.subjects })) throw IllegalArgumentException("Invalid subjects field")if (!courseNum.isNullOrEmpty()) {
+            if (subject.isNullOrEmpty()) { throw IllegalArgumentException("Course num is specified without a subject") }
+            if (subject.size > 1) { throw IllegalArgumentException("Course num inputted with too many subjects") }
+        }
         if (!delivery.isNullOrEmpty() && !delivery.all { it in fields.deliveryTypes }) throw IllegalArgumentException("Delivery types empty or invalid")
         if (!startEndTime.isNullOrEmpty() && startEndTime.size != 2) throw IllegalArgumentException("StartEndTime empty or doesnt have size 2")
         if (!(openWaitlist.isNullOrEmpty() || (openWaitlist.isNotEmpty() && openWaitlist in fields.waitlistTypes))) throw IllegalArgumentException("Invalid openWaitlist field")
